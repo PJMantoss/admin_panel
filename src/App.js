4e8bcd6d44408046574c7fa8e2d2e098e2c29f17
PJMantoss <joelptoss@gmail.com>
2022-07-12 19:41:55 +0100

    update App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TopBar from './components/topbar/TopBar';
 import Sidebar from './components/sidebar/Sidebar';
+import './app.css';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <TopBar />
       <div className="container">
         <Sidebar />
+        <div className="others">Other Pages</div>
       </div>
     </div>
   );
