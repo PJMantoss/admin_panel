3f7b4a94110648ce73b5fed9e27f1f9705779928
PJMantoss <joelptoss@gmail.com>
2022-07-12 19:37:07 +0100

    update Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import TopBar from './components/topbar/TopBar';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div>
       <TopBar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
