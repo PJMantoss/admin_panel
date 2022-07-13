b6c08a8c3b02a6cdf4f5060ff36a3baa671df163
PJMantoss <joelptoss@gmail.com>
2022-07-13 17:37:02 +0100

    update FeaturedInfo component

--- src/components/featuredInfo/FeaturedInfo.js
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -7,7 +7,21 @@ function FeaturedInfo() {
         <div className='featuredItem'>
             <span className='featuredTitle'>Revenue</span>
             <div className='featuredMoneyContainer'>
-                <span className='featuredMoney'>$</span>
+                <span className='featuredMoney'>$2,540</span>
+                <span className='featuredMoneyRate'>
+                    -12.58
+                    <ArrowDownwardOutlined />
+                </span>
+            </div>
+            <span className='featuredSubCat'>
+                Compared to last month
+            </span>
+        </div>
+
+        <div className='featuredItem'>
+            <span className='featuredTitle'>Sales</span>
+            <div className='featuredMoneyContainer'>
+                <span className='featuredMoney'>$2,540</span>
                 <span className='featuredMoneyRate'>
                     -12.58
                     <ArrowDownwardOutlined />
