119a524fa940e3cd2cd38a0316dcf8b8eb4b04d2
PJMantoss <joelptoss@gmail.com>
2022-07-13 17:28:55 +0100

    add FeaturedInfo component

---
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FeaturedInfo() {
+  return (
+    <div className='featured'>
+        <div className='featuredItem'>
+            <span className='featuredTitle'></span>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedInfo
