393eef4c6a09be593bb8bc8410aa777bf515496b
PJMantoss <joelptoss@gmail.com>
2022-07-13 19:04:03 +0100

    update Widget component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,13 +3,18 @@ import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import Chart from '../../components/chart/Chart';
 import { userData } from '../../dummyData';
 import './home.css';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
 
 function Home() {
   return (
     <div className='home'>
         <FeaturedInfo />
         <Chart data={userData} title='User Analytics' dataKey='Active User' grid />
-        <div className='homeWidgets'></div>
+        <div className='homeWidgets'>
+            <WidgetSm />
+            <WidgetLg />
+        </div>
     </div>
   )
 }
