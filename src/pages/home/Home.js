229f34083d13cd08b9f5edeb6ce5dd390fae4ed7
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:16:43 +0100

    update App

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import Chart from '../../components/chart/Chart';
 import './home.css';
 
 function Home() {
   return (
     <div className='home'>
         <FeaturedInfo />
+        <Chart />
     </div>
   )
 }
