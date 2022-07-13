a3dfeb801d16b654bd7ec38c5223f95daee041d2
PJMantoss <joelptoss@gmail.com>
2022-07-13 19:11:42 +0100

    update Widget components

--- src/components/widgetSm/WidgetSm.js
+++ src/components/widgetSm/WidgetSm.js
@@ -6,7 +6,9 @@ function WidgetSm() {
     <div className='widgetSm'>
         <span className='widgetSmTitle'>New Members</span>
         <ul className='widgetSmList'>
-            <li className='widgetSmListItem'></li>
+            <li className='widgetSmListItem'>
+                <img src='' alt='' className='widgetSmImg' />
+            </li>
         </ul>
     </div>
   )
