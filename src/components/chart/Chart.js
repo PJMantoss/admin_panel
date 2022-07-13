3d9675887e2b8ce53fc385576d55be16438b8910
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:50:55 +0100

    update Chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -7,7 +7,7 @@ function Chart({title, data, dataKey, grid}) {
 
   return (
     <div className='chart'>
-        <h3 className='chartTitle'>User Analytics</h3>
+        <h3 className='chartTitle'>{title}</h3>
         <ResponsiveContainer width="100%" aspect={4/1}>
             <LineChart data={data}>
                 <XAxis dataKey='name' stroke='#5550bd' />
