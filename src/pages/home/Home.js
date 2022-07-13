2c558302eb1f77fac8af7e660781a2d095150648
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:52:22 +0100

    update Chart

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import Chart from '../../components/chart/Chart';
+import { userData } from '../../dummyData';
 import './home.css';
 
 function Home() {
   return (
     <div className='home'>
         <FeaturedInfo />
-        <Chart />
+        <Chart data={userData} title='User Analytics' dataKey='Active User' grid />
     </div>
   )
 }
