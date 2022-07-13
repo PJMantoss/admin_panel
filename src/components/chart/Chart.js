8d8c3cbc1f0be2056ec05e372dc84fc506918986
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:19:04 +0100

    update App

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -3,7 +3,9 @@ import './chart.css';
 
 function Chart() {
   return (
-    <div className='chart'>Chart</div>
+    <div className='chart'>
+        <h3 className='chartTitle'>Sales Analytics</h3>
+    </div>
   )
 }
 
