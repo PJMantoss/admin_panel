d0c804dcea8f25a0af29930896be9aa8e2ef9f21
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:37:37 +0100

    update Chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -11,41 +11,49 @@ function Chart() {
           'Active User': 4000,
         },
         {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
+            name: 'Feb',
+            'Active User': 3000,
         },
         {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
+            name: 'Mar',
+            'Active User': 2000,
+          },
+          {
+              name: 'Apr',
+              'Active User': 1200,
+          },
+          {
+            name: 'May',
+            'Active User': 1900,
+          },
+          {
+              name: 'Jun',
+              'Active User': 2200,
+          },
+          {
+              name: 'Jul',
+              'Active User': 3000,
+            },
+            {
+                name: 'Aug',
+                'Active User': 3500,
+            },
+            {
+                name: 'Sep',
+                'Active User': 4000,
+              },
+              {
+                  name: 'Oct',
+                  'Active User': 4000,
+              },
+              {
+                  name: 'Nov',
+                  'Active User': 3000,
+                },
+                {
+                    name: 'Dec',
+                    'Active User': 2000,
+                },
       ];
 
   return (
