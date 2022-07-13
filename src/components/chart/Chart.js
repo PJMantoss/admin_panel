e9fb2b2fc9df81cc3b50b325f6174ffb22587ed9
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:50:26 +0100

    update Chart

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,64 +5,13 @@ import './chart.css';
 
 function Chart({title, data, dataKey, grid}) {
 
-    const data = [
-        {
-          name: 'Jan',
-          'Active User': 4000,
-        },
-        {
-            name: 'Feb',
-            'Active User': 3000,
-        },
-        {
-            name: 'Mar',
-            'Active User': 2000,
-          },
-          {
-              name: 'Apr',
-              'Active User': 1200,
-          },
-          {
-            name: 'May',
-            'Active User': 1900,
-          },
-          {
-              name: 'Jun',
-              'Active User': 2200,
-          },
-          {
-              name: 'Jul',
-              'Active User': 3000,
-            },
-            {
-                name: 'Aug',
-                'Active User': 3500,
-            },
-            {
-                name: 'Sep',
-                'Active User': 4000,
-                },
-                {
-                    name: 'Oct',
-                    'Active User': 4000,
-                },
-                {
-                    name: 'Nov',
-                    'Active User': 3000,
-                },
-                {
-                    name: 'Dec',
-                    'Active User': 2000,
-                },
-      ];
-
   return (
     <div className='chart'>
         <h3 className='chartTitle'>User Analytics</h3>
         <ResponsiveContainer width="100%" aspect={4/1}>
             <LineChart data={data}>
                 <XAxis dataKey='name' stroke='#5550bd' />
-                <Line type='monotone' dataKey='Active User' stroke='#5550bd' />
+                <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
                 <Tooltip />
                 {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='4 4' />}
             </LineChart>
---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: 'Jan',
+      'Active User': 4000,
+    },
+    {
+        name: 'Feb',
+        'Active User': 3000,
+    },
+    {
+        name: 'Mar',
+        'Active User': 2000,
+      },
+      {
+          name: 'Apr',
+          'Active User': 1200,
+      },
+      {
+        name: 'May',
+        'Active User': 1900,
+      },
+      {
+          name: 'Jun',
+          'Active User': 2200,
+      },
+      {
+          name: 'Jul',
+          'Active User': 3000,
+        },
+        {
+            name: 'Aug',
+            'Active User': 3500,
+        },
+        {
+            name: 'Sep',
+            'Active User': 4000,
+            },
+            {
+                name: 'Oct',
+                'Active User': 4000,
+            },
+            {
+                name: 'Nov',
+                'Active User': 3000,
+            },
+            {
+                name: 'Dec',
+                'Active User': 2000,
+            },
+  ];
