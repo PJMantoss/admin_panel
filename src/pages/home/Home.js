9f8d16160f702f359ac1425228cd74e1fabd1edc
PJMantoss <joelptoss@gmail.com>
2022-07-13 18:55:15 +0100

    update Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,6 +9,7 @@ function Home() {
     <div className='home'>
         <FeaturedInfo />
         <Chart data={userData} title='User Analytics' dataKey='Active User' grid />
+        <div className='homeWidgets'></div>
     </div>
   )
 }
