65e77fcfc70a0aeed0a420853203d5f53748f149
PJMantoss <joelptoss@gmail.com>
2022-07-14 17:09:30 +0100

    update large Widget component

--- src/components/widgetLg/WidgetLg.js
+++ src/components/widgetLg/WidgetLg.js
@@ -7,6 +7,18 @@ function WidgetLg() {
       <h3 className='widgetLgTitle'>
         Latest Transactions
       </h3>
+
+      <table className='widgetLgTable'>
+        <thead>
+          <tr className='widgetLgTr'>
+            <th className='widgetLgTh'>Customer</th>
+            <th className='widgetLgTh'>Date</th>
+            <th className='widgetLgTh'>Amount</th>
+            <th className='widgetLgTh'>Status</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   )
 }
