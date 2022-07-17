566cb87c135247e433230cf39f91d88066d44cfc
PJMantoss <joelptoss@gmail.com>
2022-07-17 17:56:15 +0100

    update NewUser page

--- src/pages/newUserPage/NewUser.js
+++ src/pages/newUserPage/NewUser.js
@@ -3,7 +3,18 @@ import './newUser.css';
 
 function NewUser() {
   return (
-    <div className='newUser'>NewUser</div>
+    <div className='newUser'>
+      <h1 className='newUserTitle'>New User</h1>
+      <form className='newUserForm'>
+        <div className='newUserItem'>
+          <label>Username</label>
+          <input 
+            type='text' 
+            placeholder='jon' 
+          />
+        </div>
+      </form>
+    </div>
   )
 }
 
