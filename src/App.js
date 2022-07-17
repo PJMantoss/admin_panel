10687e182af84cf1231d5528f184d787eba5b1c4
PJMantoss <joelptoss@gmail.com>
2022-07-17 19:25:23 +0100

    add Product page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUserPage/NewUser';
 import ProductList from './pages/Products/ProductList';
+import Product from './pages/product/Product';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './app.css';
 
@@ -20,7 +21,7 @@ function App() {
           <Route path='/user/:userId' element={<User />} />
           <Route path='/newuser' element={<NewUser />} />
           <Route path='/products' element={<ProductList />} />
-          <Route path='/product/:productId' element={<User />} />
+          <Route path='/product/:productId' element={<Product />} />
           <Route path='/product' element={<NewUser />} />
         </Routes>
       </div>
