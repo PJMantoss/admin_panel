c3b62e21e6c8f0b0d40a95e5242317ecc05c2f76
PJMantoss <joelptoss@gmail.com>
2022-07-17 19:04:30 +0100

    update ProductList page

--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -7,6 +7,55 @@ import './productList.css';
 
 function ProductList() {
     const [data, setData] = useState(productRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter(item => item.id !== id));
+    };
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'product', headerName: 'Product', width: 200, renderCell: (params) => {
+            return(
+                <div className='productListProduct'>
+P                  <img className='productListImg' src={params.row.img} alt='' />
+                    {params.row.name}
+                </div>
+            )
+        } 
+        },
+        { field: 'stock', headerName: 'Stock', width: 130 },
+        {
+          field: 'status',
+          headerName: 'Status',
+          width: 90,
+        },
+        {
+            field: 'transaction',
+            headerName: 'Transaction Volume',
+            width: 150,
+        },
+        {
+            field: 'action',
+            headerName: 'Action',
+            width: 150,
+            renderCell: (params) => {
+                return(
+                    <>
+                        <Link to={'/product/'+params.row.id}>
+                            <button className='productListEdit'>
+                                Edit
+                            </button>
+                        </Link>
+                        <DeleteOutlinedIcon 
+                            className='productListDelete' 
+                            onClick={() => handleDelete(params.row.id)} 
+                        />
+                    </>
+                )
+            }
+        },
+      ];
+
   return (
     <div className='productList'>
         <DataGrid
