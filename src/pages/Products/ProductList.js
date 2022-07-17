440536a65c9cec0524ac18000aa277dac6b094d5
PJMantoss <joelptoss@gmail.com>
2022-07-17 18:51:19 +0100

    update ProductList page

--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import { userRows } from '../../dummyData';
+import { Link } from 'react-router-dom';
 import './productList.css';
 
 function ProductList() {
   return (
-    <div className='productList'>ProductList</div>
+    <div className='productList'>
+        <DataGrid
+            rows={data}
+            columns={columns}
+            pageSize={8}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableSelectionOnClick
+        />
+    </div>
   )
 }
 
