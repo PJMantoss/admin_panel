17e264cc835113bdf63c00e7abef3c4ad8cb4c8e
PJMantoss <joelptoss@gmail.com>
2022-07-19 10:01:21 +0100

    update newProduct page

--- src/pages/newProduct/NewProduct.js
+++ src/pages/newProduct/NewProduct.js
@@ -3,7 +3,35 @@ import './newProduct.css';
 
 function NewProduct() {
   return (
-    <div className='newProduct'>NewProduct</div>
+    <div className='newProduct'>
+        <h1 className='addProductTitle'>New Product</h1>
+        <form className='addProductForm'>
+            <div className='addProductItem'>
+                <label>Image</label>
+                <input type='file' id='file' />
+            </div>
+
+            <div className='addProductItem'>
+                <label>Name</label>
+                <input type='text' placeholder='Jacket' />
+            </div>
+
+            <div className='addProductItem'>
+                <label>Stock</label>
+                <input type='text' placeholder='123' />
+            </div>
+
+            <div className='addProductItem'>
+                <label>Acitve</label>
+                <select name='active' id='active'>
+                    <option value='yes'>Yes</option>
+                    <option value='no'>No</option>
+                </select>
+            </div>
+                <button className='productBtn'>Create</button>
+            </div>
+        </form>
+    </div>
   )
 }
 
